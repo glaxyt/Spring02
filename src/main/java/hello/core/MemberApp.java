9a5393080c61070c7f134d75143a8c069ace2951
glaxyt <glaxyt@naver.com>
2025-01-11 01:59:43 +0900

    section04

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -2,6 +2,9 @@ package hello.core;
 
 import java.sql.SQLOutput;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
@@ -9,7 +12,13 @@ import hello.core.member.MemberServiceImpl;
 
 public class MemberApp {
 	public static void main(String[] args) {
-		MemberService memberService = new MemberServiceImpl();
+		// AppConfig appConfig = new AppConfig();
+		// MemberService memberService = appConfig.memberService();
+
+		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+		// @Bean은 찾으려는 객체의 이름을 메서드의 이름으로 등록을한다.
+		MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
 		Member member = new Member(1L, "memberA", Grade.VIP);
 		memberService.join(member);
 
